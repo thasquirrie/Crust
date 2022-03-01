5f230a7da9a6c279cdd79b2f0067c74d8ef6c102
thasquirrie <thasquirrie@gmail.com>
2022-03-01 20:13:58 +0100

    Added form update on save and continue

--- frontend/src/components/BusinessDetails.jsx
+++ frontend/src/components/BusinessDetails.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import { createForm } from '../actions/formActions';
 import Uploading from './Uploading';
+import SuccessAlert from './SuccessAlert';
+import ErrorALert from './ErrorAlert';
 
 const industryTypeOptions = [
   'Hospitality',
@@ -93,7 +95,11 @@ const BusinessDetails = ({}) => {
   const navigate = useNavigate();
 
   const formCreate = useSelector((state) => state.formCreate);
-  const { loading: createLoading, success: createSuccess } = formCreate;
+  const {
+    loading: createLoading,
+    success: createSuccess,
+    error: createError,
+  } = formCreate;
 
   console.log({ formCreate });
 
@@ -741,14 +747,46 @@ const BusinessDetails = ({}) => {
             </div>
           </div>
           <div className='px-4 py-5 bg-gray-50 text-right sm:px-6'>
-            <button
-              type='submit'
-              className='bg-slate-600 border border-transparent rounded-md shadow-3xl py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 drop-shadow-xl'
-            >
-              Save and Continue
-            </button>
+            {createLoading ? (
+              <button
+                type='button'
+                className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-slate-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 hover:drop-shadow-lg hover:-translate-y-1 active:-translate-y-1'
+                disabled
+              >
+                <svg
+                  className={`animate-spin -ml-1 mr-3 h-4 w-4 text-white`}
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                >
+                  <circle
+                    className='opacity-25'
+                    cx='12'
+                    cy='12'
+                    r='10'
+                    stroke='currentColor'
+                    strokeWidth='4'
+                  ></circle>
+                  <path
+                    className='opacity-75'
+                    fill='currentColor'
+                    d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                  ></path>
+                </svg>
+                Signing you in
+              </button>
+            ) : (
+              <button
+                type='submit'
+                className='bg-slate-600 border border-transparent rounded-md shadow-3xl py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500 drop-shadow-xl'
+              >
+                Save and Continue
+              </button>
+            )}
           </div>
         </div>
+        {createSuccess && <SuccessAlert action={'Save successful'} />}
+        {createError && <ErrorALert error={createError} />}
       </form>
     </div>
   );
